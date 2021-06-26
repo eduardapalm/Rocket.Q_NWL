807c6266bb2e9db600b40360d48a77959e7b4aee
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-26 17:02:30 -0300

    Update: room/questions verification

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -27,5 +27,34 @@ module.exports = {
     }
     await db.close();
     res.redirect(`/room/${roomId}`);
+  },
+
+  async open(req, res){
+    const db = await Database();
+    const roomId = req.params.room;
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`);
+    let noQuestions
+
+    if(questions.length == 0){
+      if(questionsRead.length == 0){
+        noQuestions = true
+      }
+    }
+    res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead,noQuestions: noQuestions})
+  },
+
+  async enter(req, res){
+    const roomId = req.body.roomId;
+    const db = await Database();
+
+    const verifyRoom = await db.get(`SELECT id FROM rooms WHERE id=${roomId}`);
+
+    if(verifyRoom){
+      res.redirect(`/room/${roomId}`);
+    } else{
+      res.render('dontExists', {roomId: roomId});
+    }
   }
+  
 }
