811e5314450842706be4fb140ed5549dc552b6b3
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-26 17:06:13 -0300

    Update: fixing routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
+
 const express = require('express');
-const questionController = require("./controllers/questionController");
-const roomController = require("./controllers/roomController");
+const questionController = require('./controllers/questionController');
+const roomController = require('./controllers/roomController');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
 routes.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}));
 
-routes.get('/room/:room', (req, res) => res.render("room"));
-
-routes.post('/questions/:room/:question/:action', questionController.index);
+routes.get('/room/:room', roomController.open);
 routes.post('/create-room', roomController.create);
+routes.post("/enter-room", roomController.enter)
+
+routes.post('/question/:room/:question/:action', questionController.index);
+routes.post('/question/create/:room', questionController.create)
 
 module.exports = routes;
