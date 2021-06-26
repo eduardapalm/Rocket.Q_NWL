f1613f3414696c7c02aa11315f2842ceb7c7946d
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-25 21:41:49 -0300

    Update: adding the post route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const questionController = require("./controllers/questionController");
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.render("index"));
 routes.get('/room', (req, res) => res.render("room"));
 routes.get('/create-pass', (req, res) => res.render("create-pass"));
+
+routes.post('/room/:room/:question/:action', questionController.index);
 
 module.exports = routes;
