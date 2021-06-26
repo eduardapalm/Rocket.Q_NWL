97475a82c8c370c4d4890be44a1877db78b59538
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-25 22:42:06 -0300

    Update: editing index files through the routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ const questionController = require("./controllers/questionController");
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.render("index"));
+routes.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
 routes.get('/room', (req, res) => res.render("room"));
-routes.get('/create-pass', (req, res) => res.render("create-pass"));
+routes.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}));
 
 routes.post('/room/:room/:question/:action', questionController.index);
+routes.post('/room/create-room', roomController.create);
 
 module.exports = routes;
