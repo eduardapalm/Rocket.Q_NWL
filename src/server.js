b9a9cfad48be364202ed28ec27fc48d556793808
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-25 22:44:03 -0300

    Update: encoding url through an middleware

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ server.use(express.static("public"));
 
 server.set('views', path.join(__dirname, 'views'));
 
+server.use(express.urlencoded({extended: true}))
+
 server.use(routes);
 
 server.listen(3000, () => console.log("SERVIDOR RODANDO"));
