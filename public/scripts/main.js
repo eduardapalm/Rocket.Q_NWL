46c4f9840e34c4e17b6d7b349813bc447800d806
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-25 21:41:05 -0300

    Update: getting information from the html page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,6 +9,13 @@ const checkButtons = document.querySelectorAll(".actions a.check");
 
 function handleClick(event, check = true){
   const text = check ? "Marcar como Lida" : "Excluir";
+  const slug = check ? "check" : "delete";
+  const roomId = document.querySelector("#room-id").dataset.id;
+  const questionId = event.target.dataset.id;
+  
+  const form = document.querySelector(".modal form");
+  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+  
   modalTitle.innerHTML = `${text}`;
   modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`;
   modalButton.innerHTML = `Sim, ${text.toLowerCase()}!`;
